7321398140c817e6113e143758026de4a65802c1
uaman89 <uaman89@gmail.com>
2020-08-30 13:40:52 +0300

    feat: do catalog upload, add .env

--- src/sync/Sync.js
+++ src/sync/Sync.js
@@ -1,23 +1,29 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import axios from 'axios';
+import { API_HOST } from '../constants';
 
 export function Sync() {
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
+      const data = new FormData();
+      data.append('catalog', file);
+      axios.post(`${API_HOST}/upload`, data, {
+      })
+        .then(
+          res => {
+            console.log(res.statusText);
+            alert('catalog has been uploaded');
 
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-      // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
-      }
-      reader.readAsArrayBuffer(file)
+          },
+          (err) => {
+            alert('error');
+            console.error(err);
+          });
     })
-    
+
   }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
   return (
     <div {...getRootProps()}>
